98da8dfccbbdba43b1dabce0fa98a4a65df79b40
Meri <smt5783@gmail.com>
2023-04-12 03:43:00 +0800

    優化、修復bug

--- all.js
+++ all.js
@@ -1,22 +1,10 @@
-/*  完成條件
-    1. TodoList: 可新增和刪除待辦事項。
-    2. TodoList: 待辦事項會有狀態（完成與否），可透過 checkbox 來切換。
-    3. TodoList: 待辦清單列表會有『全部』、『完成』、『未完成』Tab 來做篩選切換。
-    4. TodoList: 可以清除全部已完成功能。
-    5. TodoList: 會顯示出待完成的待辦有幾項。 */
-
-// TodoList資料 (陣列 -> 物件)
+// todolist (陣列 -> 物件)
 let data = [];
-// DOM
-const addInput = document.querySelector(".add_input");
-const addBtn = document.querySelector(".btn_add");
-const tab = document.querySelector(".tab");
-const list = document.querySelector(".list");
-const todoNum = document.querySelector(".todo_num");
-const clearList = document.querySelector(".clear_list");
 
-// 渲染資料
-function render(showData = data) {
+// 1.渲染資料
+const list = document.querySelector(".list");
+
+function render(showData) {
   let str = "";
   showData.forEach((i) => {
     str += `<li data-id="${i.id}">
@@ -24,72 +12,92 @@ function render(showData = data) {
                 <input type="checkbox" ${i.status}>
                 <span>${i.content}</span>
               </label>
-              <a href="#" class="delete">🅧</a>
+              <a href="#" class="delete">X</a>
             </li>`;
   });
   list.innerHTML = str;
-  // 待完成項目數
-  todoNum.textContent = data.filter((i) => i.status === "").length;
 }
 
-// 新增 List
+// 2.更新資料
+const todoNum = document.querySelector(".todo_num");
+let status = "all";
+
+function update() {
+  let showList = [];
+  if (status === "done") {
+    showList = data.filter((i) => i.status === "checked");
+  } else if (status === "undone") {
+    showList = data.filter((i) => i.status !== "checked");
+  } else {
+    showList = data;
+  }
+  // 待完成項目數
+  todoNum.textContent = data.filter((i) => i.status === "").length;
+  render(showList);
+}
+
+// 3.新增待辦事項
+const addInput = document.querySelector(".add_input");
+const addBtn = document.querySelector(".btn_add");
+
 function addTodo() {
   let newTodo = {
     id: new Date().getTime(),
     content: addInput.value.trim(),
-    status: "",
+    status: ""
   };
   if (addInput.value.trim() === "") {
     alert("待辦事項不能留白～");
     return;
   }
   data.push(newTodo);
-  render();
-  addInput.value = ""; // 重置 input 欄位
+  update();
+  addInput.value = "";
 }
+// 點擊按鈕、Enter鍵：新增待辦
 addBtn.addEventListener("click", () => addTodo());
 addInput.addEventListener("keyup", (e) =>
-  e.key == "Enter" ? addTodo() : null
+  e.key === "Enter" ? addTodo() : null
 );
 
-// 刪除 List
+// 4.刪除 List、切換 checkbox
 list.addEventListener("click", (e) => {
   let id = e.target.closest("li").dataset.id;
-  if (e.target.getAttribute("class") == "delete") {
+  let itemIndex = data.findIndex((i) => i.id == id);
+  if (e.target.getAttribute("class") === "delete") {
     e.preventDefault();
-    let dataIndex;
-    data.forEach((i, index) => (i.id == id ? (dataIndex = index) : null));
-    data.splice(dataIndex, 1);
-    render();
+    data.splice(itemIndex, 1);
+    update();
   } else {
-    data.forEach((i) => {
-      if (i.id != id) return;
-      i.status === "checked" ? (i.status = "") : (i.status = "checked");
-    });
-    render();
+    data[itemIndex].status =
+      data[itemIndex].status === "checked" ? "" : "checked";
   }
+  update();
 });
 
-// 清除已完成 List
+// 5.清除已完成項目
+const clearList = document.querySelector(".clear_list");
+
 clearList.addEventListener("click", (e) => {
   e.preventDefault();
-  // data 重新賦予篩選的資料
-  data = data.filter((i) => i.status !== "checked");
-  render();
+  // data 賦予 篩選”待完成資料“
+  data = data.filter((i) => i.status === "");
+  update();
 });
 
-// 切換 Tab
+// 6.切換 Tab
+const tabs = document.querySelectorAll(".tab li");
+const tab = document.querySelector(".tab");
+
 tab.addEventListener("click", (e) => {
-  let tabs = document.querySelectorAll(".tab li");
-  let status = e.target.dataset.status;
-  let filterData = data;
-
+  status = e.target.dataset.status;
   tabs.forEach((i) => i.classList.remove("active"));
-  if (status === "done") {
-    filterData = data.filter((i) => i.status === "checked");
-  } else if (status === "undone") {
-    filterData = data.filter((i) => i.status !== "checked");
-  }
   e.target.classList.add("active");
-  render((showData = filterData));
+  update();
 });
+
+// 優化建議：
+// ✅ 在已完成頁籤下取消勾選待辦事項後，沒有重新渲染（待完成事項不應出現在已完成頁籤）
+// ✅ 更改勾選狀態的部分可透過 data[itemIndex].status 將狀態反轉，不需再跑 forEach
+// ✅ 新增待辦事項後切到 "待完成" 頁籤，再切到 "全部" 的頁籤會顯是空的（不應是空的）
+// ✅ 在待完成頁籤下勾選待辦事項後會渲染全部的待辦事項（應只渲染待完成的待辦事項）
